5f9a932c5ad71652cdb6ebfb78ae302234bd9976
Utsav9163 <utsavkumargupta89@gmail.com>
2025-02-16 03:54:32 +0530

    gsap to nav

--- static/frontend/script.js
+++ static/frontend/script.js
@@ -23,15 +23,14 @@ h4all.forEach(function (elem) {
 });
 
 gsap.to("#nav", {
-  backgroundColor: "#000",
-  duration: 0.5,
-  height: "110px",
-  scrollTrigger: {
-    trigger: "#nav",
-    scroller: "body",
-    // markers:true,
-    start: "top -10%",
-    end: "top -11%",
-    scrub: 1,
-  },
-});
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "110px",
+    scrollTrigger: {
+      trigger: "#nav",
+      scroller: "body",
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+    },
+  });
